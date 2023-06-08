6d66f737543b19c4086c4ddf021766c80187b484
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2023-06-08 12:08:14 +1200

    added heaps more

--- tests/toSvg-basic.test.ts
+++ tests/toSvg-basic.test.ts
@@ -14,11 +14,14 @@ function expectSnapshot(json: object) {
    actual = pd.xml(actual)
    const testname = expect.getState().currentTestName
    const dir = './tests/__out__/'
-   const filename = dir + testname.replace(/ /g, '_') + '.html'
+   const htmlFile = dir + testname.replace(/ /g, '_') + '.html'
+   const svgFile = dir + testname.replace(/ /g, '_') + '.svg'
 
    !fs.existsSync(dir) && fs.mkdirSync(dir)
-   fs.existsSync(filename) && fs.unlinkSync(filename)
-   fs.writeFileSync(filename, actual)
+   fs.existsSync(htmlFile) && fs.unlinkSync(htmlFile)
+   fs.writeFileSync(htmlFile, actual)
+   fs.existsSync(svgFile) && fs.unlinkSync(svgFile)
+   fs.writeFileSync(svgFile, actual)
 
    expect(actual).toMatchSnapshot()
 }
